899df7f0cd45971780dee1d19205b1d49aefd046
Eleanor CM <97481864+EleanorCM@users.noreply.github.com>
2024-10-28 16:03:03 +0000

    Solved challenge 1 - added new tour

--- tour-service/src/main/java/com/example/Application.java
+++ tour-service/src/main/java/com/example/Application.java
@@ -3,6 +3,8 @@ package com.example;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.example.tourservice.Tour;
+import com.example.tourservice.TourManagementService;
 import com.example.tourservice.TravelAgentService;
 
 public class Application {
@@ -11,6 +13,9 @@ public class Application {
 
         ApplicationContext context = new AnnotationConfigApplicationContext(Application.class.getPackageName());
         TravelAgentService agent = context.getBean(TravelAgentService.class);
+        TourManagementService tourManagementService = context.getBean(TourManagementService.class);
+        tourManagementService.createTour("Zoo Tour", 100, true);
+
         System.out.println("\n******Explore California Tour Catalogue******");
         agent.displayTours();
 
